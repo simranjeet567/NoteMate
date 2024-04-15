57f0bd954d56213585724882a61976e5e643d10c
Simranjeet <simranjeet777000@gmail.com>
2024-04-15 22:15:40 +0530

    making login page and setting up email and password functionalities in it.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const  Navbar = () => {
+  return (
+    <div className='bg-white flex items-center justify-betwee px-6 drop-shadow'>
+        <h2 className='text-xl font-medium text-black py-2'>NoteMate</h2>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme:{
+    extend:{
+      colors:{
+        primary:"#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
+  },
   plugins: [],
-}
+};
 
