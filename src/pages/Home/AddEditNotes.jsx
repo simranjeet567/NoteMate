43536495d4e3599775a1ea6c93a525882717a31a
Simranjeet <simranjeet777000@gmail.com>
2024-04-17 11:46:29 +0530

    making cards for the notes structure

---
+++ src/pages/Home/AddEditNotes.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AddEditNotes = () =>  {
+  return (
+    <div>
+        <div className='flex flex-col gap-2'>
+            <label className='input-label'>TITLE</label>
+            <input
+                type='text'
+                className='tex-2xl text-slate-950 outline-none'
+                placeholder='Go To Gym At 6AM'
+            />
+        </div>
+
+        <div className='flex flex-col gap-2 mt-4'>
+            <label className='input-label'>CONTENT</label>
+            <textarea
+                type='text'
+                className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
+                placeholder='Content'
+                rows={10}
+                />
+        </div>
+        </div>
+  );
+};
+
+export default AddEditNotes;
